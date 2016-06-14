a45ae1b01013ebf854038ac4c8eacf33d3ca8a4c
hugsy <hugsy@blah.cat>
2016-06-14 17:14:46 +1000

    AddHeadersForWafBypass: fixed bad copy/paste

--- 9AddHeadersForWafBypass.lua
+++ 9AddHeadersForWafBypass.lua
@@ -1,12 +1,18 @@
 AUTHOR = "hugsy"
-PLUGIN_NAME = "AddHeader"
+PLUGIN_NAME = "AddHeadersForWafBypass"
+
+function proxenet_on_load()
+end
+
+function proxenet_on_leave()
+end
 
 function proxenet_request_hook (request_id, request, uri)
    local CRLF = "\r\n"
    local header = "X-Originating-IP: 127.0.0.1" .. CRLF
    header = header .. "X-Forwarded-For: 127.0.0.1" .. CRLF
    header = header .. "X-Remote-IP: 127.0.0.1" .. CRLF
-   header = header .. "X-Remote-Addr: 127.0.0.1" .. CRLF
+   header = header .. "X-Remote-Addr: 127.0.0.1"
 
    return string.gsub(request,
                       CRLF .. CRLF,
@@ -16,4 +22,3 @@ end
 function proxenet_response_hook (response_id, response, uri)
    return response
 end
-Next  Previous
